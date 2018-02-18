eb0c0b301d513aa1d051cc39ec25a2cba6ce78e6
dj <dj22@gmx.com>
2018-02-18 16:05:37 +0100

    My first website

---
+++ build_site.R
@@ -0,0 +1,8 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/home/dj-ubuntu/Documents/Webpage/personal_webpage")
+
+#render your sweet site. 
+rmarkdown::render_site()
